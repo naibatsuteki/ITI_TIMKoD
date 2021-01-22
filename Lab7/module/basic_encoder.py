a94400a1f8fe48887d2ea78a415302dd3f520ab1
Sebastian Niespodziany <=>
2021-01-22 11:51:33 +0100

    Add gitignore and lab7 project structure.

---
+++ Lab7/module/basic_encoder.py
@@ -0,0 +1,10 @@
+from abc import abstractmethod
+
+class BasicEncoder:
+    
+    @abstractmethod
+    def funcname(self, parameter_list):
+        """
+        docstring
+        """
+        raise NotImplementedError
